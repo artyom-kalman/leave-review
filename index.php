33f0cb5c4e8e5f0031164c5f0167d7529b826171
artyom-kalman <p.a.kalashnikov@ya.ru>
2024-02-28 23:02:25 +1100

    Update index.php: add dbConnection and context

--- index.php
+++ index.php
@@ -1,23 +1,20 @@
 <?php  
 
-// require 'vendor/autoload.php';
+require('Data/DbConnection.php');
+require('Data/DbContext.php');
 
-$conn_string = "host=localhost dbname=rate-page user=postgres password=12345";
-$dbconn = pg_connect($conn_string);
-if (!$dbconn) {
-    echo("Failde to connect to database");
-    return;
-}
-else {
-    // echo("Connection is succesfuly established");
-}
+use RatePage\Data\DbContext;
+
+$connectionString = "host=localhost dbname=rate-page user=postgres password=12345";
+$dbConnection = connectToDb($connectionString);
+
+$dbContext = new DbContext($dbConnection);
+
+require 'src/routes.php';
 
 $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $params = $_GET;
 
-// echo($_GET["username"]);
-
-require 'src/routes.php';
 $router->dispatch($uri, $params);
 
 ?>
