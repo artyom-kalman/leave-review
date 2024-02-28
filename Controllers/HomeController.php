b2f5b9f59046966125c4ab32f9144c56de5a50a6
artyom-kalman <p.a.kalashnikov@ya.ru>
2024-02-28 17:34:38 +1100

    Update HomeController.php: redirect if no username params

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -7,6 +7,8 @@ use RatePage\Data\Controller;
 include "Models/Review.php";
 use RatePage\Models\Review;
 
+
+
 class HomeController extends Controller {
     public function index() {
         $this->render('Home/index', "Home");
@@ -16,7 +18,12 @@ class HomeController extends Controller {
         $this->render('Home/error', "Что-то пошло не так");
     }
 
-    public function newReview() {
+    public function newReview($params = []) {
+        if (!array_key_exists("username", $params)) {
+            header('Location: error');
+            exit;
+        }
+        echo($params['username']);
         $this->render('Home/newReview', "Оставьте отзыв");
     }
 }
