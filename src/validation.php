32c5878270d31ce5b26034a96658950957c4026e
artyom-kalman <p.a.kalashnikov@ya.ru>
2024-03-03 15:11:33 +1000

    Update validation: add length check and regex check

--- src/validation.php
+++ src/validation.php
@@ -1,6 +1,12 @@
 <?php
 
-function validateTextInput($input): string {
+function validateTextInput($input): string|false {
+    if (!preg_match_all("/[а-яА-Я .ё?,!0-9]+/m", $input) ) {  
+        return false; 
+    }
+    if (strlen($input) < 4) {
+        return false;
+    }
     $result = trim($input);
     return $result;
 }
